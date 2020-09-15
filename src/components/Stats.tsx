e882cf92ec8b9daf55332601f8001c5a200a441b
mars <mars@marss-MBP.tx.rr.com>
2020-09-14 22:53:55 -0500

    added useEffect

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,63 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import useSWR, { mutate } from "swr";
 import { HabitSWR, HabitJSON } from "../types";
 
 export function Stats() {
-  const {habitName} = useParams()
-  let habitStats: { [name: string]: Array<string>} = { }
-  let fuckMe: { [habitDate: string]: number} = { }
+  const { habitName } = useParams()
+
+  const [habitStatsReformat, setHSR] = useState({} as { [habitDate: string]: number })
   const { data, error }: HabitSWR = useSWR("http://localhost:3000/habit", (url) =>
     axios.get(url, { withCredentials: true }).then((res) => res.data)
   );
+  useEffect(() => {
+    let habitStats: { [name: string]: Array<string> } = {}
+    if (!data)
+      return
+    for (const key of data) {
+      if (!(key.name in habitStats)) {
+        var arr = [];
+        arr.push(key.completeDate.toString().slice(0, 10))
+        habitStats[key.name] = arr
+      } else {
+        habitStats[key.name].push(key.completeDate.toString().slice(0, 10))
+      }
+    }
+
+    habitStats[habitName].sort()
+    for (const index in habitStats[habitName]) {
+      if (!(habitStats[habitName][index] in habitStatsReformat)) {
+        habitStatsReformat[habitStats[habitName][index]] = 1
+      } else {
+        habitStatsReformat[habitStats[habitName][index]]++
+      }
+    }
+    setHSR({ ...habitStatsReformat })
+  }, [data])
+
   if (!data) {
     return <div>Loading...</div>;
   }
   if (error) {
     return <div>Error...</div>;
   }
-  // create useStats hook
-  // put this in useStats hook and return the transformed data
-  // useStats instead of useSWR
-  // sstore result of transformation with useState inside the useStats hook
-  // https://swr.vercel.app/getting-started
-  for (const key of data) {
-    if (!(key.name in habitStats)) {
-      var arr = [];
-      arr.push(key.completeDate.toString().slice(0,10))
-      habitStats[key.name] = arr
-    } else {
-      habitStats[key.name].push(key.completeDate.toString().slice(0,10))
-    }
-  }
 
-  habitStats[habitName].sort()
-
-  for (const index in habitStats[habitName]) {
-    if (!(habitStats[habitName][index] in fuckMe)) {
-      fuckMe[habitStats[habitName][index]] = 1
-    } else {
-      fuckMe[habitStats[habitName][index]] ++
-    }
-  }
-
-  console.log(fuckMe)
   return <div>
     <div className="habit-stats">
-    {
-      Object.entries(fuckMe).map(([key, val], i) => (
-        <p>
-          <span>Date: {key} </span>
-          <span>
-            Completed: 
+      {
+        Object.entries(habitStatsReformat).map(([key, val], i) => (
+          <p key={i}>
+            <span>Date: {key} </span>
+            <span>
+              Completed:
           </span>
-          {[...Array(val)].map((e, k) => 
-            <span className="points" key={k}>+</span>
-          )}
-        </p>
-      ))
-    }
+            {[...Array(val)].map((e, k) =>
+              <span className="points" key={k}>+</span>
+            )}
+          </p>
+        ))
+      }
     </div>
-    </div>;
+  </div>
 }
+
+
+
+
+
